343003b5bb83d9591837274f568ec722c09855f1
YooJini <wlsdl6685@gmail.com>
2020-06-15 22:24:42 +0900

    플레이어 생성  1) 플레이어 이동
     - 유니티버전: Unity 2019.3.13f1(64-bit)
     - 비주얼스튜디오: 2019 community

---
+++ New Unity Project (1)/Assets/_JINI/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5.0f;  //이동 속도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
